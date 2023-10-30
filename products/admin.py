7f26db2f49159e6b03fee339a88cfc5cd493f390
salms358 <sa083850@gmail.com>
2023-10-30 13:41:13 +0000

    Adding a many to many relationship to the models so more than one category can be chosen to put the products in

--- products/admin.py
+++ products/admin.py
@@ -1,19 +1,24 @@
 from django.contrib import admin
 from .models import Product, Category
 
-# Register your models here.
-
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'sku',
         'name',
-        'category',
+        'display_categories',  # Replace 'category' with 'display_categories'
         'price',
         'rating',
         'image',
     )
-
+    
     ordering = ('sku',)
+
+    def display_categories(self, obj):
+        """Display all categories associated with a product."""
+        return ", ".join([category.name for category in obj.categories.all()])
+
+    # This sets the column header for 'display_categories' method in the admin panel
+    display_categories.short_description = 'Categories'
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
