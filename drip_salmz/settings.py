20cd14298170873e64ca257e74cc95d9631a63d6
salms358 <sa083850@gmail.com>
2023-11-08 21:48:46 +0000

    the sizing for sneakers and clothes both work and now im getting the sizes to show up in the shopping bag

--- drip_salmz/settings.py
+++ drip_salmz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [  '8000-salms358-dripbysalmz-yxddj8i653j.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = [ '8000-salms358-dripbysalmz-yxddj8i653j.ws-eu106.gitpod.io']
 
 
 # Application definition
--- products/models.py
+++ products/models.py
@@ -22,10 +22,21 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    product_size = models.CharField(
+        max_length=2,
+        null=True,
+        blank=True,
+    )
+    shoe_size = models.BooleanField(
+        max_length=3,
+        null=True,
+        blank=True,
+    )
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
 
     def __str__(self):
         return self.name
