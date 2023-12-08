37cd8f5e8466e1c4ad24b84a6222e62cbf3fdcf5
salms358 <sa083850@gmail.com>
2023-12-08 15:25:45 +0000

    url structure fixed

--- drip_salmz/settings.py
+++ drip_salmz/settings.py
@@ -33,7 +33,7 @@ DEBUG = False
 
 #ALLOWED_HOSTS = [ 'dripzs-by-salmz-006e3a5f6351.herokuapp.com', 'localhost']
 
-ALLOWED_HOSTS = [ 'https://dripzs-by-salmz-006e3a5f6351.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [ 'dripzs-by-salmz-006e3a5f6351.herokuapp.com', 'localhost']
 
 
 # Application definition
