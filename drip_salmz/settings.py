a79e5677f721c43661d7f210cc51d8cfbaee30ee
salms358 <sa083850@gmail.com>
2023-12-08 11:31:04 +0000

    switxhing branches

--- drip_salmz/settings.py
+++ drip_salmz/settings.py
@@ -29,10 +29,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [ 'dripzs-by-salmz-006e3a5f6351.herokuapp.com', 'localhost']
+#ALLOWED_HOSTS = [ 'dripzs-by-salmz-006e3a5f6351.herokuapp.com', 'localhost']
 
+ALLOWED_HOSTS = [ '8000-salms358-dripbysalmz-yxddj8i653j.ws-eu106.gitpod.io']
 
 
 # Application definition
@@ -191,6 +192,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+# trying to send cloudinary files to a custom folder
+# DEFAULT_FILE_STORAGE = 'custom_storage.CustomCloudinaryStorage'
 
 
 
