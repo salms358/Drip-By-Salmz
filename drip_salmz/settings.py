48188dc80f8da6474e24b38dfb665dff0521e9d7
salms358 <sa083850@gmail.com>
2023-12-08 11:40:33 +0000

    debugset to false and the main branch is back to normal

--- drip_salmz/settings.py
+++ drip_salmz/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['dripzs-by-salmz-006e3a5f6351.herokuapp.com', 'localhost']
 
