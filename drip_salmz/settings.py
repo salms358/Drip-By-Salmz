51c04e448d1c79d9462fec448d6e7ed98cf90c88
salms358 <sa083850@gmail.com>
2023-12-14 13:20:41 +0000

    changing the padding and in the process of creating a sitemapand robots.txt file

--- drip_salmz/settings.py
+++ drip_salmz/settings.py
@@ -29,11 +29,11 @@ DEBUG1 = 'DEVELOPMENT' in os.environ
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-#ALLOWED_HOSTS = [ 'dripzs-by-salmz-006e3a5f6351.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [ 'dripzs-by-salmz-006e3a5f6351.herokuapp.com', 'localhost']
 
-ALLOWED_HOSTS = [ '8000-salms358-dripbysalmz-yxddj8i653j.ws-eu106.gitpod.io']
+#ALLOWED_HOSTS = [ '8000-salms358-dripbysalmz-yxddj8i653j.ws-eu106.gitpod.io']
 
 
 
